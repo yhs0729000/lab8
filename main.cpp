c966fdcff59d77444f2929f1a7a865b4c8b4127e
yhs0729000 <yhs072900000@gmail.com>
2017-04-05 22:01:09 +0800

    try14

--- main.cpp
+++ main.cpp
@@ -1,42 +1,75 @@
-#include <iostream>
+#include<iostream>
+#include<stdio.h>
 #include<string>
-#include<cstdio>
-#include"time.h"
+#include "timer6.h"
 using namespace std;
-char mapID[22][52],mapHP[22][52];
+
 string to_string(int val){
 	char str[10];
 	sprintf(str,"%d",val);
 	return string(str);
 }
-void update(){
-	
-	int sec,mana,one=1,two,three,four;
-	char a;
-    cin>>sec>>mana>>one>>two>>three>>four;
-    for(int i=0;i<22;i++){
-    	for(int j=0;j<52;j++)
-    		cin.get(mapID[i][j]);
-    	cin.get(a);
-	}
-	for(int i=0;i<22;i++){
-		for(int j=0;j<52;j++)
-			cin.get(mapHP[i][j]);
-		cin.get(a);
-	}
-	
-	cout<<string("1 ")+to_string(one)+ " 5 23"<<endl;
-	cout<<string("2 ")+to_string(two)+ " 17 23"<<endl;
-	/*cout<<string("1 ")+to_string(three)+ " 5 23"<<endl;
-	cout<<string("1 ")+to_string(four)+ " 5 23"<<endl;*/
-	cout<<0<<endl;
-}
-int main(){
-	cout<<"3 4 7 1 8 6 2 5"<<endl;
-	while(true){
-		update();
-	}
-    //cin>>sec>>mana>>one>>two>>three>>four;
+//抽卡排序//
+class drawcard{
 
-} 
+
+
+public:
+
+	void printcard(){ cout<<"7 5 4 1 6 3 8 2"<<endl; }
+
+
+
+
+};
+
+
+
+
+int main(){
+while(true)
+{	char sec,mana,one,two,three,four,a;
+        drawcard draw;
+
+        draw.printcard();
+
+
+	const int row =25;
+ 	const int column = 55;
+
+	char map1[row][column];
+
+	char map2[row][column];
+
+	
+
+	//讀map//
+	cin>>sec>>mana>>one>>two>>three>>four;
+
+	int i,j;
+		for(i=0;i<22;i++)
+		{
+	         for(j=0;j<52;j++)
+			 {
+				 cin.get(map1[i][j]);
+				 
+			
+			 }
+		}
+
+		for(i=0;i<22;i++)
+		{
+	         for(j=0;j<52;j++)
+			 {
+				 cin.get(map2[i][j]);
+			 }
+		}
+
+               cout<<string("1")+to_string(one)+ " 6 22"<<endl;
+
+		
+		return 0;
+
+}
+}
 		
